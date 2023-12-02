cdf9c195961c2526cddfe111bf8a00120be687e3
cesar <cesar.rodriguezg@usantotomas.edu.co>
2023-12-01 19:33:23 -0500

    se agrega servicio de calendario

--- src/Components/CalendarModule.jsx
+++ src/Components/CalendarModule.jsx
@@ -1,20 +1,48 @@
 import React, { useState } from 'react';
 import DateRangePicker from './DateRangePicker';
+import ServiceCalendario from '../Services/ServiceCalendario';
+import { useHistory } from "react-router";
 
 const CalendarModule = () => {
-  const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
+  let history = useHistory();
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
   const [observaciones, setObservaciones] = useState('');
 
-  const handleFormSubmit = (e) => {
+  const handleFormSubmit = async (e) => {
     e.preventDefault();
-    
+  
     // Aquí puedes enviar los datos al servidor o realizar cualquier acción necesaria
     const data = {
       startDate,
       endDate,
-      observaciones
+      observaciones,
+      Estado: 'Pendiente',
     };
+  
+    try {
+      const response = await ServiceCalendario.CargarDias(data);
+      if(response.Estado==true)
+      {
+        window.alert("Se agrego con exito sus dias de permiso");
+        setStartDate('');
+        setEndDate('');
+        setObservaciones('');
+        history.push('/Home');
+        window.location.reload();
+      }
+      if(response.Estado==false)
+      {
+        window.alert("no se puedo realizar su peticion");
+        setStartDate('');
+        setEndDate('');
+        setObservaciones('');
+      }
+      console.log('Respuesta del servidor:', response);
+    } catch (error) {
+      console.error('Error al enviar los datos al servidor:', error.message);
+    }
+  
     console.log('Datos enviados:', data);
   };
 
@@ -26,7 +54,7 @@ const CalendarModule = () => {
           <label>Fecha de inicio:</label>
           <input
             type="date"
-            value={startDate}
+            value={startDate || ''}
             onChange={(e) => setStartDate(e.target.value)}
           />
         </div>
@@ -34,14 +62,14 @@ const CalendarModule = () => {
           <label>Fecha de fin:</label>
           <input
             type="date"
-            value={endDate}
+            value={endDate || ''}
             onChange={(e) => setEndDate(e.target.value)}
           />
         </div>
         <div className="form-group">
           <label>Observaciones de sus vacaciones</label>
           <textarea
-            value={observaciones}
+            value={observaciones || ''}
             onChange={(e) => setObservaciones(e.target.value)}
           />
         </div>
---
+++ src/Services/ServiceCalendario.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+const ServiceCalendario = {
+  CargarDias: async (data) => {
+    try {
+      const sessionString = localStorage.getItem('session');
+      const sessionObject = JSON.parse(sessionString);
+      const token = sessionObject.token;
+      const id=sessionObject.id;
+      data.id = id;
+      const response = await axios.post('http://localhost:3023/apidos/AgregarDias', data, // Pasa los datos del nuevo usuario como el cuerpo de la solicitud
+      {
+        headers: {
+          'authorization': `${token}`,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      throw new Error('Error al cargar días en el calendario');
+    }
+  },
+};
+export default ServiceCalendario;
