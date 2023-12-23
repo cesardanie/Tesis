649ba083b8272b1948f76993a9811957194a4721
cesar <cesar.rodriguezg@usantotomas.edu.co>
2023-12-23 00:25:08 -0500

    se agrega firma servicio de firma

--- src/Services/ServiceFirma.js
+++ src/Services/ServiceFirma.js
@@ -3,25 +3,28 @@ import axios from 'axios';
 const ServiceFirma = {
   PostFirma: async (firmaBase64) => {
     try {
-        const sessionString = localStorage.getItem('session');
-        const sessionObject = JSON.parse(sessionString);
-        const token = sessionObject.token;
-        const response = await axios.post('http://localhost:3023/apiseis/FirmaInsert',{
-            headers: {
-                'authorization': `${token}`,
-                'Content-Type': 'application/json',
-               
-              }
-              ,body: JSON.stringify({ firmaBase64 }),
-              
+      const sessionString = localStorage.getItem('session');
+      const sessionObject = JSON.parse(sessionString);
+      const token = sessionObject.token;
+      const id = sessionObject.id;
 
-        });
-        console.log(response.data);
-        return response.data;
-      } catch (error) {
-        throw new Error('Error al obtener Firma');
-      }
-  }, 
+      const response = await axios.post(
+        'http://localhost:3023/apiseis/FirmaInsert',
+        { id, firmaBase64 },
+        {
+          headers: {
+            'authorization': token,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      throw new Error('Error al obtener Firma');
+    }
+  },
 };
 
 export default ServiceFirma;
