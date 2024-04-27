6142d44ee1cfe480ef2807b28017b012fb22879a
cesar <cesar.rodriguezg@usantotomas.edu.co>
2024-04-27 17:00:12 -0500

    se agregan dependendicas

--- src/Services/AuthService.js
+++ src/Services/AuthService.js
@@ -6,7 +6,7 @@ const AuthService = {
       Correo=Correo.toLowerCase();
       Correo=CryptoJS.AES.encrypt(Correo,`${process.env.REACT_APP_MY_KEY}`).toString();
       Contrasena=CryptoJS.AES.encrypt(Contrasena,`${process.env.REACT_APP_MY_KEY}`).toString();    
-      const response = await axios.post('http://localhost:3023/api/IniciarSesion', {
+      const response = await axios.post(`https://backentesis-c23c16c1c921.herokuapp.com/api/IniciarSesion`, {
         Correo,
         Contrasena
       });
