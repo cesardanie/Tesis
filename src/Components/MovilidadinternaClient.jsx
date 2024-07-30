48429f8c5b615060713396573582aec6312ac91c
cesar <cesar.rodriguezg@usantotomas.edu.co>
2024-07-30 00:29:12 -0500

    se agrega movilidad interna get funcional

--- src/Components/MovilidadinternaClient.jsx
+++ src/Components/MovilidadinternaClient.jsx
@@ -1,25 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, Container, Row, Col, Button } from 'react-bootstrap';
 import '../Estilos/MovilidadinternaClient.css';
 import { useHistory } from "react-router";
-
-const ofertas = [
-    { id: 1, titulo: "Oferta 1", descripcion: "Descripción de la oferta 1" },
-    { id: 2, titulo: "Oferta 2", descripcion: "Descripción de la oferta 2" },
-    // Agrega más ofertas según sea necesario
-];
+import ServicioMovilidad from '../../../Tesis/src/Services/ServiciodeMovilidadInterna';
 
 const MovilidadinternaClient = () => {
+    const [oferta, setofertas] = useState([]);
     let history = useHistory();
+
+    useEffect(() => {
+        const cargarUsuarios = async () => {
+            try {
+                const data = await ServicioMovilidad.GetOfertas();
+                setofertas(data); // Asume que data es un array con las ofertas
+            } catch (error) {
+                console.error('Error al cargar usuarios:', error.message);
+            }
+        };
+
+        cargarUsuarios();
+    }, []); // El segundo argumento [] asegura que se ejecute solo al montar el componente
+
     const handleApply = (offer) => {
-        console.log(`Aplicar a la oferta: ${offer.titulo}`);
+        console.log(`Aplicar a la oferta: ${offer.TituloOferta}`);
         // Agrega tu lógica para aplicar a la oferta
     };
 
     const handleReject = (offer) => {
-        console.log(`Rechazar la oferta: ${offer.titulo}`);
+        console.log(`Rechazar la oferta: ${offer.TituloOferta}`);
         // Agrega tu lógica para rechazar la oferta
     };
+
     const RedireccionarMenu = () => {
         history.push('/Home');
         window.location.reload();
@@ -29,12 +40,12 @@ const MovilidadinternaClient = () => {
         <Container className="movilidad-container">
             <h1 className="text-center my-4">Ofertas de Movilidad Interna</h1>
             <Row>
-                {ofertas.map((offer) => (
-                    <Col key={offer.id} md={6} lg={4} className="mb-4">
+                {oferta.map((offer) => (
+                    <Col key={offer.Id} md={6} lg={4} className="mb-4">
                         <Card className="offer-card">
                             <Card.Body>
-                                <Card.Title>{offer.titulo}</Card.Title>
-                                <Card.Text>{offer.descripcion}</Card.Text>
+                                <Card.Title>{offer.TituloOferta}</Card.Title>
+                                <Card.Text>{offer.Descripcion}</Card.Text>
                                 <div className="button-container">
                                     <Button variant="primary" className="me-2 apply-button" onClick={() => handleApply(offer)}>
                                         Aplicar
@@ -52,7 +63,6 @@ const MovilidadinternaClient = () => {
             <br />
             <br />
         </Container>
-
     );
 };
 
